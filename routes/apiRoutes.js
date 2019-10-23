2fd6550d301b57df7bcbde204b8159a4726c8441
Carla Manosa <carlamanosa@yahoo.com>
2019-10-23 09:02:57 -0700

    updated js routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,18 +1,44 @@
-var noteData = require("../db/db.json");
-var path = require("path");
-// var index = require("../public/index.html");
-// var notes = require("../public/notes.html");
-module.exports = function(app) {
-  app.get("/api/notes", function(req, res) {
-    res.json(noteData);
+const router = require("express").Router();
+const fs = require("fs");
+​
+​
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+​
+​
+​
+  router.get("/notes", function(req, res) {
+    fs.readFile("db/db.json", "utf8", (err, jsonString) =>{
+      if (err) {
+        console.log("file read failed: ", err);
+        return;
+      }
+      res.json(JSON.parse(jsonString));
+    });
   });
-  app.post("/api/notes", function(req, res) {
-      noteData.push(req.body);
-      res.json(true);
+​
+  router.post("/notes", ({ body }, res) => {
+    const rawdata = fs.readFileSync("db/db.json");
+    const parsedata = JSON.parse(rawdata);
+    const newObj = parsedata.concat(body);
+    const string = JSON.stringify(newObj);
+    fs.writeFile("db/db.json", string, function(err){
+      if(err) console.log(err);
+      res.json(string);
+    });
   });
-  app.post("/api/clear", function(req, res) {
-    index.length = 0;
-    notes.length = 0;
-    res.json({ ok: true });
+​
+  router.delete("/notes/:title", function(req, res){
+    const rawdata = fs.readFileSync("db/db.json");
+    const parsedata = JSON.parse(rawdata);
+    const newData = parsedata.filter(obj => {
+      return obj.title !== req.params.title;
+    });
+    fs.writeFile("db/db.json", JSON.stringify(newData), function(err){
+      if(err) console.log(err);
+      res.json(newData);
+    });
   });
-};
+​
+  module.exports = router; 
